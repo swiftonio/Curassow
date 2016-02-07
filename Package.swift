fd33581f153cfd5b7d635d767d9fc17cc5a7e34e
Saulius Grigaitis <saulius@necolt.com>
2016-02-07 18:19:11 +0200

    Dependencies: use Swifton forks

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ import PackageDescription
 let package = Package(
   name: "Curassow",
   dependencies: [
-    .Package(url: "https://github.com/nestproject/Nest.git", majorVersion: 0, minor: 2),
-    .Package(url: "https://github.com/nestproject/Inquiline.git", majorVersion: 0, minor: 2),
+    .Package(url: "https://github.com/swiftonio/Nest.git", majorVersion: 0, minor: 2),
+    .Package(url: "https://github.com/swiftonio/Inquiline.git", majorVersion: 0, minor: 2),
     .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0, minor: 4),
   ],
   testDependencies: [
